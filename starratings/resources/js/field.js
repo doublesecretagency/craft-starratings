1554f5f5e297f0f04b04de4b252f25d9e83dc5a8
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2017-02-25 23:46:50 -0800

    Partial vanilla JS

--- starratings/resources/js/field.js
+++ starratings/resources/js/field.js
@@ -1,24 +1,46 @@
-$('body').on('click', '.sr-star', function () {
+function hasClass(el, className) {
+	if (el instanceof Element) {
+		return el.className.split(' ').indexOf(className) > -1;
+	} else {
+		return false;
+	}
+}
 
-	// Get info
-	var starValue = $(this).data('rating');
-	var $allStars = $(this).parent('div').children('.sr-star');
+document.addEventListener('click', function (e) {
 
-	// Mark selected stars
-	$allStars.each(function () {
-		$(this).removeClass('sr-user-rating sr-unrated');
-		$(this).find('i').removeClass('fa-star fa-star-o');
-		var starRating = $(this).data('rating');
-		if (starRating <= starValue) {
-			$(this).addClass('sr-user-rating');
-			$(this).find('i').addClass('fa-star');
-		} else {
-			$(this).addClass('sr-unrated');
-			$(this).find('i').addClass('fa-star-o');
+	// Get elements
+	var fa   = e.target;
+	var star = fa.parentNode;
+
+	// Check click target
+	if (hasClass(star, 'sr-star')) {
+
+		var starValue = star.getAttribute('data-rating');
+		var starGroup = star.parentNode.children;
+
+		// console.log(starGroup);
+
+		for (i in starGroup) {
+			var s = starGroup[i];
+			if (hasClass(s, 'sr-star')) {
+				console.log(s);
+			}
 		}
-	});
 
-	// Set input value
-	$(this).siblings('input').val(starValue);
+	}
 
 });
+
+// 		$(this).removeClass('sr-user-rating sr-unrated');
+// 		$(this).find('i').removeClass('fa-star fa-star-o');
+// 		var starRating = $(this).data('rating');
+// 		if (starRating <= starValue) {
+// 			$(this).addClass('sr-user-rating');
+// 			$(this).find('i').addClass('fa-star');
+// 		} else {
+// 			$(this).addClass('sr-unrated');
+// 			$(this).find('i').addClass('fa-star-o');
+// 		}
+
+// 	// Set input value
+// 	$(this).siblings('input').val(starValue);
