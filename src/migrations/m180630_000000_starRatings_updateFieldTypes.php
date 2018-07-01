87156b0e00a9561dd1303c60fde6c65479b4129a
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2018-06-30 22:02:45 -0700

    Fieldtype migration

---
+++ src/migrations/m180630_000000_starRatings_updateFieldTypes.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Star Ratings plugin for Craft CMS
+ *
+ * An easy to use and highly flexible ratings system.
+ *
+ * @author    Double Secret Agency
+ * @link      https://www.doublesecretagency.com/
+ * @copyright Copyright (c) 2015 Double Secret Agency
+ */
+
+namespace doublesecretagency\starratings\migrations;
+
+use craft\db\Migration;
+
+use doublesecretagency\starratings\fields\AvgUserRating;
+use doublesecretagency\starratings\fields\Rate;
+
+/**
+ * Migration: Update field types for Craft 3 compatibility
+ * @since 2.1.0
+ */
+class m180630_000000_starRatings_updateFieldTypes extends Migration
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        // Auto-update existing Rate fields
+        $this->update('{{%fields}}', [
+            'type' => Rate::class
+        ], [
+            'type' => 'StarRatings_Rate'
+        ], [], false);
+
+        // Auto-update existing AvgUserRating fields
+        $this->update('{{%fields}}', [
+            'type' => AvgUserRating::class
+        ], [
+            'type' => 'StarRatings_AvgUserRating'
+        ], [], false);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m180630_000000_starRatings_updateFieldTypes cannot be reverted.\n";
+
+        return false;
+    }
+
+}
